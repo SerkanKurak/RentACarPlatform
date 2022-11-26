7a700193107c06b17f1157e70968a0db7c28b105
Serkankurak@students.softuni.bg <Serkankurak@students.softuni.bg>
2022-11-26 23:19:47 +0200

    Added Protection

---
+++ RentACarPlatform.Infrastructure/Data/Models/Protection.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static RentACarPlatform.Infrastructure.Data.Constants.DataConstants.Protection;
+
+namespace RentACarPlatform.Infrastructure.Data.Models
+{
+    public class Protection
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(NameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public string Coverage { get; set; } = null!;
+
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
